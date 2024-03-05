10342efbb245aa628307474da738d3a7f2750d9c
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2024-03-05 17:03:28 -0500

    set up get_player_stats method/route

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, request
+from methods.player import get_player_stats
 
 app = Flask(__name__)
 
 @app.route('/api', methods=['POST'])
 def player_stats():
   if request.method == 'POST':
-    return
+    return get_player_stats()
--- methods/player.py
+++ methods/player.py
@@ -1,5 +1,9 @@
+import os
 import requests
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def get_player_stats():
-  response = requests.get('')
-  return
+  response = requests.get(f'https://api.brawlhalla.com/player/3/stats?api_key={os.getenv("API_KEY")}')
+  return response.text
