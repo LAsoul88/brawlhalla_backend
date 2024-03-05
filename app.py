d9ddbfd9da5779dfaabb554e578d94b8c13bf3c1
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2024-03-05 17:50:39 -0500

    configured CORS and further refined get_player_stats parameters

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request
+from flask_cors import CORS
 from methods.player import get_player_stats
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/api', methods=['POST'])
 def player_stats():
   if request.method == 'POST':
-    data = request.get_json()
-    return get_player_stats(data)
+    player_id = request.get_json()
+    return get_player_stats(player_id)
--- methods/player.py
+++ methods/player.py
@@ -4,6 +4,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-def get_player_stats(data):
-  response = requests.get(f'https://api.brawlhalla.com/player/{data["player_id"]}/stats?api_key={os.getenv("API_KEY")}')
+def get_player_stats(player_id):
+  response = requests.get(f'https://api.brawlhalla.com/player/{player_id}/stats?api_key={os.getenv("API_KEY")}')
   return response.text
