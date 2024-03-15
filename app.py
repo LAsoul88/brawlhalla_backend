6530b026046c4e2b81e3b9716be9327c38f80ebf
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2024-03-14 22:34:57 -0700

    changed get_player_stats to get_player_ranked

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 from flask_cors import CORS
-from methods.player import get_legends, get_player_stats
+from methods.player import get_legends, get_player_ranked
 
 app = Flask(__name__)
 CORS(app, origins=['http://localhost:3000'])
@@ -12,4 +12,4 @@ def brawlhalla_api():
 
   if request.method == 'POST':
     player_id = request.get_json()
-    return get_player_stats(player_id)
+    return get_player_ranked(player_id)
--- methods/player.py
+++ methods/player.py
@@ -8,6 +8,6 @@ def get_legends():
   response = requests.get(f'https://api.brawlhalla.com/legend/all?api_key={os.getenv("API_KEY")}')
   return response.text
 
-def get_player_stats(player_id):
-  response = requests.get(f'https://api.brawlhalla.com/player/{player_id}/stats?api_key={os.getenv("API_KEY")}')
+def get_player_ranked(player_id):
+  response = requests.get(f'https://api.brawlhalla.com/player/{player_id}/ranked?api_key={os.getenv("API_KEY")}')
   return response.text
