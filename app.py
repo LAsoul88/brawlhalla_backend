9b02596f5658fc61f147840e97bf1123b94bdd3d
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2024-03-18 23:09:27 -0700

    converted api calls functions into class

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
 from flask import Flask, request
 from flask_cors import CORS
-from methods.player import get_legends, get_player_ranked
+from methods.player import BrawlhallaAPI
 
 app = Flask(__name__)
 CORS(app, origins=['http://localhost:3000'])
 
-@app.route('/api', methods=['GET', 'POST'])
+@app.route('/api', methods=['POST'])
 def brawlhalla_api():
-  if request.method == 'GET':
-    return get_legends()
 
   if request.method == 'POST':
-    player_id = request.get_json()
-    return get_player_ranked(player_id)
+    data = request.get_json()
+    comms = BrawlhallaAPI(data['endpoint'], data['player_id'])
+    return comms.direct_call()
--- methods/player.py
+++ methods/player.py
@@ -4,10 +4,28 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-def get_legends():
-  response = requests.get(f'https://api.brawlhalla.com/legend/all?api_key={os.getenv("API_KEY")}')
-  return response.text
+class BrawlhallaAPI:
+  def __init__(self, endpoint, player_id = None):
+    self.endpoint = endpoint
+    self.player_id = player_id
 
-def get_player_ranked(player_id):
-  response = requests.get(f'https://api.brawlhalla.com/player/{player_id}/ranked?api_key={os.getenv("API_KEY")}')
-  return response.text
+  def direct_call(self):
+    match self.endpoint:
+      case 'legend':
+        return self.__get_legends()
+      case 'ranked':
+        return self.__get_player_ranked()
+      case 'stats':
+        return self.__get_player_stats()
+
+  def __get_legends(self):
+    response = requests.get(f'https://api.brawlhalla.com/legend/all?api_key={os.getenv("API_KEY")}')
+    return response.text
+
+  def __get_player_ranked(self):
+    response = requests.get(f'https://api.brawlhalla.com/player/{self.player_id}/ranked?api_key={os.getenv("API_KEY")}')
+    return response.text
+
+  def __get_player_stats(self):
+    response = requests.get(f'https://api.brawlhalla.com/player/{self.player_id}/stats?api_key={os.getenv("API_KEY")}')
+    return response.text
