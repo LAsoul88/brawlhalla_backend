cc0fd3504e296159d7f55e01c7abb777e0d2f6cc
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2024-03-20 16:25:03 -0700

    added player to 'stats' call, fixed response to be in json format

--- methods/player.py
+++ methods/player.py
@@ -1,5 +1,7 @@
 import os
 import requests
+import json
+
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -14,16 +16,22 @@ class BrawlhallaAPI:
       case 'legend':
         return self.__get_legends()
       case 'stats':
-        return {'stats': self.__get_player_stats(), 'ranked': self.__get_player_ranked()}
+        stats = self.__get_player_stats()
+        ranked = self.__get_player_ranked()
+        player = {
+          'name': stats['name'] or ranked['name'],
+          'brawlhalla_id': self.player_id,
+        }
+        return {'player': player, 'stats': stats, 'ranked': ranked}
 
   def __get_legends(self):
-    response = requests.get(f'https://api.brawlhalla.com/legend/all?api_key={os.getenv("API_KEY")}')
-    return response.text
+    response = requests.get(f'https://api.brawlhalla.com/legend/all?api_key={os.getenv("API_KEY")}').json()
+    return response
 
   def __get_player_ranked(self):
-    response = requests.get(f'https://api.brawlhalla.com/player/{self.player_id}/ranked?api_key={os.getenv("API_KEY")}')
-    return response.text
+    response = requests.get(f'https://api.brawlhalla.com/player/{self.player_id}/ranked?api_key={os.getenv("API_KEY")}').json()
+    return response
 
   def __get_player_stats(self):
-    response = requests.get(f'https://api.brawlhalla.com/player/{self.player_id}/stats?api_key={os.getenv("API_KEY")}')
-    return response.text
+    response = requests.get(f'https://api.brawlhalla.com/player/{self.player_id}/stats?api_key={os.getenv("API_KEY")}').json()
+    return response
