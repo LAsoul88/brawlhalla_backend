6d61d12288eb25c0045105138498d20d989cb105
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2024-03-22 19:09:31 -0700

    renamed class to Proxy, completely changed how properties are changed, initiated, and unpacked

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 from flask_cors import CORS
-from methods.player import BrawlhallaAPI
+from methods.player import Proxy
 
 app = Flask(__name__)
 CORS(app, origins=['http://localhost:3000'])
@@ -10,5 +10,7 @@ def brawlhalla_api():
 
   if request.method == 'POST':
     data = request.get_json()
-    comms = BrawlhallaAPI(data['endpoint'], data['player_id'])
+    comms = Proxy()
+    comms.unpack(data)
+    print(comms.endpoint)
     return comms.direct_call()
--- methods/player.py
+++ methods/player.py
@@ -1,30 +1,27 @@
 import os
 import requests
-import json
 
 from dotenv import load_dotenv
 
 load_dotenv()
 
-class BrawlhallaAPI:
-  def __init__(self, endpoint, player_id = None):
-    self.endpoint = endpoint
-    self.player_id = player_id
+class Proxy:
+  def __init__(self):
+    self.endpoint = None
+    self.player_id = None
+    self.region = None
+    self.bracket = None
+    self.page = None
 
-  def direct_call(self):
-    match self.endpoint:
-      case 'legend':
-        return self.__get_legends()
-      case 'stats':
-        stats = self.__get_player_stats()
-        ranked = self.__get_player_ranked()
-        player = {
-          'name': stats['name'] or ranked['name'],
-          'brawlhalla_id': self.player_id,
-        }
-        return {'player': player, 'stats': stats, 'ranked': ranked}
-      case 'leaderboard':
-        return self.__get_leaderboard()
+  def __setitem__(self, key, value):
+    setattr(self, key, value)
+  
+  def __getitem__(self, key):
+    return getattr(self, key)
+
+  def unpack(self, data):
+    for point in data.keys():
+      self[point] = data.get(point)
 
   def __get_legends(self):
     response = requests.get(f'https://api.brawlhalla.com/legend/all?api_key={os.getenv("API_KEY")}').json()
@@ -39,5 +36,20 @@ class BrawlhallaAPI:
     return response
   
   def __get_leaderboard(self):
-    response = requests.get(f'https://api.brawlhalla.com/rankings/1v1/all/1?api_key={os.getenv("API_KEY")}').json()
+    response = requests.get(f'https://api.brawlhalla.com/rankings/{self.bracket}/{self.region}/{self.page}?api_key={os.getenv("API_KEY")}').json()
     return response
+
+  def direct_call(self):
+    match self.endpoint:
+      case 'legend':
+        return self.__get_legends()
+      case 'stats':
+        stats = self.__get_player_stats()
+        ranked = self.__get_player_ranked()
+        player = {
+          'name': stats['name'] or ranked['name'],
+          'brawlhalla_id': self.player_id,
+        }
+        return {'player': player, 'stats': stats, 'ranked': ranked}
+      case 'rankings':
+        return self.__get_leaderboard()
